1e192eee6ccc97fae78478c476c0777140626d93
moliang <596832510@domain.com>
2022-04-08 23:12:31 +0800

    初始化

---
+++ src/main/java/com/xxxx/App.java
@@ -0,0 +1,15 @@
+package com.xxxx;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    @Test
+    public void testabc(){
+        System.out.println("abc.............");
+    }
+
+}
